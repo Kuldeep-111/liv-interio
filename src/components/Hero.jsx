f17c741b9a903e1fb2c8992745dc90a5df18ad3d
kuldeep chaudhary <you@example.com>
2025-06-04 16:07:34 +0530

    mute icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Overlay from './utilities/Overlay'
 import Image from 'next/image'
 import Heading from './utilities/Heading'
@@ -7,6 +7,17 @@ import Link from 'next/link'
 import Breadcrumb from './utilities/BreadCrumb'
 
 const Hero = ({ video = false, imageSrc = '/assets/images/projects/projects-banner.webp',title ,parent}) => {
+   const videoRef = useRef(null);
+   const [isMuted, setIsMuted] = useState(true);
+ const handleUnmute = () => {
+  if (videoRef.current) {
+    const newMuted = !isMuted;
+    videoRef.current.muted = newMuted;
+    videoRef.current.play(); // always call play to resume if needed
+    setIsMuted(newMuted);
+  }
+};
+
   return (
     <section
       className="relative w-full h-[50vh] md:h-screen overflow-hidden"
@@ -14,15 +25,19 @@ const Hero = ({ video = false, imageSrc = '/assets/images/projects/projects-bann
       role="img"
     >
       {video ? (
+        <>
         <video
+          ref={videoRef}
           className="absolute top-0 left-0 w-full h-full object-cover"
           src="/assets/videos/Livinterio Version3-1.mp4"
           autoPlay
           loop
-          muted
+           muted={isMuted}
           playsInline
           
         />
+        <button  className='z-51 absolute right-[25px] bottom-[25px]' onClick={handleUnmute}><Image src={isMuted ? "/assets/icons/volume.webp" : "/assets/icons/mute.webp" }alt='Sound Icon' width={20} height={20}/></button>
+        </>
       ) : (
         <>
         <Image
